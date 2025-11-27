0397b389f5cfc9d33ca28724371a82c2000d1e13
Ujk768 <utkarsh.kanade1@gmail.com>
2025-11-27 14:02:51 -0500

    Fixed search service

--- src/main/java/com/example/ujk/finalproject/services/SearchService.java
+++ src/main/java/com/example/ujk/finalproject/services/SearchService.java
@@ -4,20 +4,17 @@ import com.example.ujk.finalproject.model.Course;
 import com.example.ujk.finalproject.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import jakarta.annotation.PostConstruct;
+
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class SearchService {
-
     @Autowired
     private CourseRepository courseRepository;
-
     private final List<Course> courses = new ArrayList<>();
     private final Map<String, Set<Integer>> invertedIndex = new ConcurrentHashMap<>();
-
 
     // Load from DB after service creation
     @PostConstruct
@@ -29,35 +26,23 @@ public class SearchService {
     private void buildIndex() {
         for (int i = 0; i < courses.size(); i++) {
             Course c = courses.get(i);
-
-            String content = (
-                    c.getCourseName() + " " +
-                            c.getCategory() + " " +
-                            c.getSubjects()
-            ).toLowerCase();
-
-            for (String word : content.split("\\W+")) {
-                invertedIndex
-                        .computeIfAbsent(word, k -> new HashSet<>())
-                        .add(i);
+            String courseName = c.getCourseName().toLowerCase();
+            if (courseName == null) continue;
+            for (String word : courseName.split("[^a-zA-Z0-9]+")) {
+                invertedIndex.computeIfAbsent(word, k -> new HashSet<>()).add(i);
             }
         }
     }
 
     public List<Course> search(String keyword) {
-        if (keyword == null || keyword.isEmpty())
-            return Collections.emptyList();
-
+        if (keyword == null || keyword.isEmpty()) return Collections.emptyList();
         keyword = keyword.toLowerCase();
-
         Set<Integer> indexes = invertedIndex.get(keyword);
         if (indexes == null) return Collections.emptyList();
-
         List<Course> results = new ArrayList<>();
         for (Integer index : indexes) {
             results.add(courses.get(index));
         }
-
         return results;
     }
 }
